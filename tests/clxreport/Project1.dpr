7cb40f3245342ea978751cb2162cf7baf1acecff
toni <toni>
2002-06-20 17:07:36 +0000

    KCompanion CD finished

--- tests/clxreport/Project1.dpr
+++ tests/clxreport/Project1.dpr
@@ -2,30 +2,7 @@ program Project1;
 
 uses
   QForms,
-  Unit1 in 'Unit1.pas' {Form1},
-{$IFDEF MSWINDOWS}
-  midaslib,
-  rpclxreport in '..\..\rpclxreport.pas',
-  rpalias in '..\..\rpalias.pas',
-  rpreport in '..\..\rpreport.pas',
-  rpdatainfo in '..\..\rpdatainfo.pas',
-  rpqtdriver in '..\..\rpqtdriver.pas' {FRpQtProgress},
-  rpcompobase in '..\..\rpcompobase.pas',
-  rppdfreport in '..\..\rppdfreport.pas',
-  rpmdesigner in '..\..\rpmdesigner.pas';
-
-{$ENDIF}
-
-{$IFDEF LINUX}
-  rpclxreport in '../../rpclxreport.pas',
-  rpalias in '../../rpalias.pas',
-  rpreport in '../../rpreport.pas',
-  rpdatainfo in '../../rpdatainfo.pas',
-  rpcompobase in '../../rpcompobase.pas',
-  rppdfreport in '../../rppdfreport.pas',
-  rpmdesigner in '../../rpmdesigner.pas';
-{$ENDIF}
-
+  Unit1 in 'Unit1.pas' {Form1};
 
 {$R *.res}
 
