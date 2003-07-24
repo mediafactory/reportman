808342c76e3741bb006ea977842c4fa301eb37d4
tonim <tonim>
2003-07-24 13:52:51 +0000

    Version

--- rpmdconsts.pas
+++ rpmdconsts.pas
@@ -35,7 +35,7 @@ const
 {$ENDIF}
 
 const
- RM_VERSION='1.6pre11';
+ RM_VERSION='1.6pre22';
  REPMAN_WEBSITE='http://reportman.sourceforge.net';
 
 function TranslateStr (index:integer; defvalue:Widestring):WideString;
