ae7bbfe732a6960f9de33f68eea1175a1844492f
tonim <tonim>
2003-08-08 18:46:56 +0000

    ActiveX update

--- activex/Reportman_TLB.pas
+++ activex/Reportman_TLB.pas
@@ -12,7 +12,7 @@ unit Reportman_TLB;
 // ************************************************************************ //
 
 // PASTLWTR : 1.2
-// File generated on 31/07/2003 17:20:52 from Type Library described below.
+// File generated on 08/08/2003 20:22:38 from Type Library described below.
 
 // ************************************************************************  //
 // Type Lib: C:\prog\toni\cvsroot\reportman\reportman\activex\ReportMan.tlb (1)
