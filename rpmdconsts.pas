7e6f926573d835b37e8e88f1864545dfbf4f0702
tonim <tonim>
2004-09-24 09:05:18 +0000

    Linux test

--- rpmdconsts.pas
+++ rpmdconsts.pas
@@ -35,7 +35,7 @@ const
 {$ENDIF}
 
 const
- RM_VERSION='2.2d';
+ RM_VERSION='2.2e';
  REPMAN_WEBSITE='http://reportman.sourceforge.net';
 
 function TranslateStr (index:integer; defvalue:Widestring):WideString;
